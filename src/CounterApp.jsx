4052011f89c4ed0b6c3d4c32a44fcdf1f7a68d13
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-21 17:26:19 -0500

    Finished CounterApp

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,7 +5,14 @@ export default function CounterApp({ value }) {
   // primer hook useState, y solo renderiza una vez con el resultado
   const [counter, setCounter] = useState(value);
   const handleAdd = (event, value) => {
-    setCounter(counter + 1 )
+    setCounter(counter + 1);
+  };
+
+  const handleSubstract = (event, value) => {
+    setCounter(counter - 1);
+  };
+  const handleReset = (event, value) => {
+    setCounter(value);
   };
   return (
     <>
@@ -14,6 +21,8 @@ export default function CounterApp({ value }) {
       {/* por default cuando se crea una funcion de ese estilo tiene como parametro un evento */}
       {/* si no le paso nada a la funcion handleAdd el toma como referencia el onclick en el mismo orden de la misma manera como el onclick lo hace por default */}
       <button onClick={(event) => handleAdd(event, value)}> +1 </button>
+      <button onClick={(event) => handleSubstract(event, value)}> -1 </button>
+      <button onClick={(event) => handleReset(event, value)}> reset</button>
     </>
   );
 }
